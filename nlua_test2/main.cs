9a2aa74a48acc3bfeaf963176907802d67bdb6e8
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-17 21:19:30 +1200

    Was trying out Nlua again and this was the result

---
+++ nlua_test2/main.cs
@@ -0,0 +1,17 @@
+﻿using NLua;
+
+Lua state = new Lua();
+string mainLuaFile = File.ReadAllText(@"./main.lua");
+state["c_sharp_methods_obj"] = new CSharpMethods();
+state.DoString(mainLuaFile);
+
+
+
+class CSharpMethods
+{
+    public string trim(string str) => str.Trim();
+	
+	public int length(string[] arr) => arr.Length; 
+	public string[] split(string str, string delimiter) => str.Split(delimiter);
+	public double clamp(double value, double min, double max) => Math.Clamp(value, min, max);
+}
