f9f106dbac3ba212ef3c689b3e261216596bb551
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-29 23:16:10 +1200

    Created a small program to count vowels in provided strings to The CountVowel method

---
+++ C#/VowelCounter.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Program
+{
+	public static int CountVowels(string str)
+    {
+    	str = str.ToLower();
+    	int vowelCount = 0;
+        
+		foreach(char c in str)
+        {
+       		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            	vowelCount++;
+            }
+        }
+        
+        return vowelCount;
+    }
+
+	public static void Main()
+	{
+    int vowelCount = CountVowels("I would like a flat white please. Also, one croissant please.");
+	  Console.WriteLine(vowelCount);
+	}
+}
