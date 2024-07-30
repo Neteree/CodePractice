57d993007e9257c4f6c90ed8055a6453b98a9625
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-31 11:44:40 +1200

    When I added this here the indentation got messed up, so I fixed it

--- C#/IsThisATriangle.cs
+++ C#/IsThisATriangle.cs
@@ -1,15 +1,14 @@
 using System;
 
-
 class Program
 {	
-   private static bool IsTriangle(int a, int b, int c)
-    {
-      return a + b > c && a + c > b && b + c > a;   
-    }
-    
-    static void Main()
-    {
+	private static bool IsTriangle(int a, int b, int c)
+	{
+		return a + b > c && a + c > b && b + c > a;   
+	}
+
+	static void Main()
+	{
 		Console.WriteLine(IsTriangle(1,2,2));
 		Console.WriteLine(IsTriangle(4,2,3));
 		Console.WriteLine(IsTriangle(2,2,2));
