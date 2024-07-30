ecd4a4e101d57ea018cbef0c12afd336856bc0fa
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-30 20:31:09 +1200

    This is an adaptation of my solution to the Palindrome Number problem on LeetCode

---
+++ C#/IsPalindrome.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+	    private static bool IsPalindrome(int xInt) {
+        
+        string xStr = xInt.ToString();
+		string reversedXStr = "";
+
+        for(int i = xStr.Length - 1; i >= 0; i--)
+        {
+            reversedXStr += xStr[i];
+        }
+
+        return xStr == reversedXStr;
+    }
+	
+	
+    static void Main()
+    {
+		Console.WriteLine(IsPalindrome(121));
+		Console.WriteLine(IsPalindrome(-121));
+		Console.WriteLine(IsPalindrome(10));
+    }
+}
