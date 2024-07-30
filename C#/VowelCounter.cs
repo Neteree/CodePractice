05c96a586c0858dd503e28bb0fba5b82391cd153
Neteree <57681573+Neteree@users.noreply.github.com>
2024-07-30 23:22:31 +1200

    Fixed indentation issues

--- C#/VowelCounter.cs
+++ C#/VowelCounter.cs
@@ -3,23 +3,23 @@ using System;
 public class Program
 {
 	public static int CountVowels(string str)
-    {
-    	str = str.ToLower();
-    	int vowelCount = 0;
+    	{
+    		str = str.ToLower();
+    		int vowelCount = 0;
         
 		foreach(char c in str)
-        {
-       		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-            	vowelCount++;
-            }
-        }
+	        {
+	       		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+	            		vowelCount++;
+	        	}
+	        }
         
-        return vowelCount;
-    }
+        	return vowelCount;
+	}
 
 	public static void Main()
 	{
-    int vowelCount = CountVowels("I would like a flat white please. Also, one croissant please.");
-	  Console.WriteLine(vowelCount);
+		int vowelCount = CountVowels("I would like a flat white please. Also, one croissant please.");
+		Console.WriteLine(vowelCount);
 	}
 }
